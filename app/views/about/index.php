e781a7a895cee29650565de83efb4f5722ce2b6e
Griswalde <agunggriswald158@gmail.com>
2021-09-30 12:56:29 +0700

    #6 Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,5 @@
-
-    <h1>about me</h1>
+<div class="container">
+    <h1 class="mt-4">about me</h1>
+    <img src="<?= BASEURL;?>/img/yoda.jpg" alt="Baby Yoda" class="rounded-circle">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>
     tahun,saya adalah seorang <?= $data['pekerjaan'] ?>. </p>
